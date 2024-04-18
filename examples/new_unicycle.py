9a5f823e89bddf10ca42e18df055229575993545
tombelv <tommaso.belv@gmail.com>
2024-04-18 15:10:45 +0200

    removed ModelJax, fixed rest

--- examples/new_unicycle.py
+++ examples/new_unicycle.py
@@ -5,27 +5,26 @@ import jax.numpy as jnp
 
 import matplotlib.pyplot as plt
 
-from sbmpc.new_model import Model, ModelJax
+from sbmpc.new_model import Model
 from sbmpc.solvers import SbMPC
 from sbmpc.utils.settings import ConfigMPC, ConfigGeneral
 import sbmpc.utils.simulation as simulation
-
 
 input_max = jnp.array([2, 4])
 input_min = -input_max
 
 
 def unicycle_dynamics(state: jnp.array, inputs: jnp.array) -> jnp.array:
-    state_dot = jnp.array([inputs[0]*jnp.cos(state[2]),
-                            inputs[0]*jnp.sin(state[2]),
-                            inputs[1]], dtype=jnp.float32)
+    state_dot = jnp.array([inputs[0] * jnp.cos(state[2]),
+                           inputs[0] * jnp.sin(state[2]),
+                           inputs[1]], dtype=jnp.float32)
     return state_dot
 
 
 def cost_fn(state: jnp.array, state_ref: jnp.array, inputs: jnp.array) -> jnp.float32:
     """ Cost function to regulate the state to the desired value"""
     error = state - state_ref
-    return 50*jnp.linalg.norm(error, ord=2) + jnp.linalg.norm(inputs, ord=2)
+    return 50 * jnp.linalg.norm(error, ord=2) + jnp.linalg.norm(inputs, ord=2)
 
 
 class Simulation(simulation.Simulator):
@@ -43,27 +42,23 @@ class Simulation(simulation.Simulator):
         self.input_traj[self.iter, :] = ctrl
 
         # Simulate the dynamics
-        self.current_state = self.model.integrate_jit(self.current_state, ctrl, self.controller.dt)
+        self.current_state = self.model.integrate(self.current_state, ctrl, self.controller.dt)
         self.state_traj[self.iter + 1, :] = self.current_state
 
 
 if __name__ == "__main__":
 
     system = Model(unicycle_dynamics, 3, 0, 2, [input_min, input_max])
-    jit_system = ModelJax(system, jax.devices('gpu')[0], "float32")
 
     x_init = jnp.array([2, 2, 0], dtype=jnp.float32)
-
-    # dummy integration
-    jit_system.integrate_jit(x_init, jnp.zeros(2), 0.0)
 
     mpc_config = ConfigMPC(0.02, 50, 0.1, num_parallel_computations=5000)
     gen_config = ConfigGeneral("float32", jax.devices("gpu")[0])
 
-    solver = SbMPC(jit_system, cost_fn, mpc_config, gen_config)
+    solver = SbMPC(system, cost_fn, mpc_config, gen_config)
 
     # Setup and run the simulation
-    sim = Simulation(x_init, jit_system, solver, 600)
+    sim = Simulation(x_init, system, solver, 600)
     sim.simulate()
 
     # Plot x-y position of the robot
--- sbmpc/new_model.py
+++ sbmpc/new_model.py
@@ -4,23 +4,27 @@ from abc import ABC, abstractmethod
 import mujoco
 from mujoco import mjx
 
+
 class BaseModel(ABC):
 
     @abstractmethod
     def integrate(self, state, inputs, dt: float):
         pass
+
     @abstractmethod
     def setInitialState(self, state):
         pass
 
+
 class Model(BaseModel):
-    def __init__(self,  model_dynamics, nq: int, nv: int, nu: int, input_bounds=(None, None)):
+    def __init__(self, model_dynamics, nq: int, nv: int, nu: int, input_bounds=(None, None)):
         self.nq = nq                        # number of generalized coordinates = dim(qpos)
         self.nv = nv                        # number of degrees of freedom = dim(qvel)
+        self.nx = nq + nv
         self.nu = nu                        # number of control inputs
         self.input_min = input_bounds[0]
         self.input_max = input_bounds[1]
-        self.state0 = jnp.zeros(nq+nv, dtype=jnp.float32) # initstate
+        self.state0 = jnp.zeros(self.nx, dtype=jnp.float32) # initstate
         self.model_dynamics = model_dynamics
 
     def setInitialState(self, state):
@@ -29,7 +33,7 @@ class Model(BaseModel):
     def dynamics(self, state, inputs):
         x_dot = self.model_dynamics(state, inputs)
         return x_dot
-    
+
     def integrate(self, state, inputs, dt: float):
         """ One-step integration of the dynamics using Rk4 method"""
         k1 = self.dynamics(state, inputs)
@@ -38,6 +42,7 @@ class Model(BaseModel):
         k4 = self.dynamics(state + k3 * dt, inputs)
         return state + (dt/6.) * (k1 + 2. * k2 + 2. * k3 + k4)
 
+
 class ModelMjx(BaseModel):
     def __init__(self, model_path, input_bounds=(None, None)):
         self.model_path = model_path
@@ -45,13 +50,14 @@ class ModelMjx(BaseModel):
         mj_model = mujoco.MjModel.from_xml_path(self.model_path)
         self.nq = mj_model.nq                        # number of generalized coordinates = dim(qpos)
         self.nv = mj_model.nv                        # number of degrees of freedom = dim(qvel)
+        self.nx = self.nq + self.nv
         self.nu = mj_model.nu                        # number of control inputs
         self.input_min = input_bounds[0]
         self.input_max = input_bounds[1]
         mj_data = mujoco.MjData(mj_model)
         self.renderer = mujoco.Renderer(mj_model)
         self.mjx_model = mjx.put_model(mj_model)
-        self.state0 = mjx.put_data(mj_model, mj_data) # initial state on the gpu
+        self.state0 = mjx.put_data(mj_model, mj_data)  # initial state on the gpu
 
     def setInitialState(self, state):
         pass
@@ -65,20 +71,19 @@ class ModelMjx(BaseModel):
         return mjx_data
 
 
-class ModelJax:
-    def __init__(self,  other_model: Model, device: jax.Device = jax.devices('cpu')[0], dtype_general="float32"):
-        self.model = other_model
-        self.device = device
-        self.dtype_general = dtype_general
-        self.input_max = self.model.input_max
-        self.input_min = self.model.input_min
-        # vectorized_integrate_jax = jax.vmap(self.integrate, in_axes=(0, 0, None), out_axes=0)
-        # self.integrate_vectorized = jax.jit(vectorized_integrate_jax, device=self.device)
-
-        self.integrate_jit = jax.jit(self.model.integrate, device=jax.devices('cpu')[0])
-
-        # Make one dummy integration to compile during construction
-        zero_ctrl = jnp.zeros(self.model.nu, dtype=dtype_general)
-        self.integrate_jit(self.model.state0, zero_ctrl, 0.0)
+# Maybe this should just become a decorator  Model and ModelMjx
+# class ModelJax:
+#     def __init__(self,  other_model: Model, device: jax.Device = jax.devices('cpu')[0], dtype_general="float32"):
+#         self.model = other_model
+#         self.device = device
+#         self.dtype_general = dtype_general
+#         # self.input_max = self.model.input_max
+#         # self.input_min = self.model.input_min
+#
+#         self.integrate = jax.jit(self.model.integrate, device=device)
+#
+#         # Make one dummy integration to compile during construction
+#         zero_ctrl = jnp.zeros(self.model.nu, dtype=dtype_general)
+#         self.integrate(self.model.state0, zero_ctrl, 0.0)
 
         
